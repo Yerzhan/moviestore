1fe528bddb681a1d69a5195a81be2f383d2ac352
sardieri <sardieri@gmail.com>
2019-01-04 22:15:24 +0300

    fetched list of trending movies from the TMDB API

--- src/components/TrendingList/TrendingList.jsx
+++ src/components/TrendingList/TrendingList.jsx
@@ -1,13 +1,47 @@
 import React from 'react';
 import { FETCH_STATUS } from '../../helpers/common/types';
+import keys from '../../config/keys';
 
 class TrendingList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       fetchStatus: FETCH_STATUS.LOADING,
-      movies: [],
+      trendings: [],
     };
+  }
+
+  componentDidMount() {
+    console.log('fetching trending movies from TMDB...');
+    this.fetchTrending();
+  }
+
+  async fetchTrending() {
+    const url = new URL(`https://api.themoviedb.org/3/trending/movie/day?api_key=${keys.TMDBapiKey}`);
+
+    try {
+      const response = await fetch(url);
+      const results = await response.json();
+      console.log(results);
+
+      this.setState({
+        fetchStatus: FETCH_STATUS.SUCCESS,
+        trendings: results.results,
+      });
+    } catch (err) {
+      this.setState({
+        fetchStatus: FETCH_STATUS.ERROR,
+      });
+      console.error(err);
+    }
+  }
+
+  renderTrending() {
+    return this.state.trendings.map(trend => (
+      <li key={trend.id}>
+        <p> {trend.title} </p>
+      </li>
+    ));
   }
 
   render() {
@@ -19,7 +53,7 @@ class TrendingList extends React.Component {
       case FETCH_STATUS.SUCCESS:
         return (
           <ul style={ulStyle}>
-            {this.renderItineraries()}
+            {this.renderTrending()}
           </ul>
         );
       default:
