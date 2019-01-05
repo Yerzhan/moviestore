2b344c244d479cc4ac0c5ed447b6295f8676b239
sardieri <sardieri@gmail.com>
2019-01-05 20:26:52 +0300

    added movie item

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import style from './MovieItem.module.scss';
 
 const MovieItem = ({ movie }) => {
+  console.log(movie);
+  const { poster_path, title, vote_average, overview } = movie;
   return (
-    <div>
-      <h2>{movie.title}</h2>
-      <p>{movie.overview}</p>
+    <div className={style.MovieItem} style={{ backgroundImage: 'url(http://image.tmdb.org/t/p/original' + poster_path + ')' }} >
+      <div className={style.overlay}>
+        <div className={style.title}>{title}</div>
+        <div className={style.rating}>{vote_average} / 10</div>
+        <div className={style.plot}>{overview}</div>
+      </div>
     </div>
   );
 }
