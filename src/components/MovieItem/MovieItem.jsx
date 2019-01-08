450942f1535f6b189dfa9e42f32b3302e7387396
sardieri <sardieri@gmail.com>
2019-01-08 17:30:30 +0300

    refactored image item to use configuration and smaller size

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import style from './MovieItem.module.scss';
+import config from '../../config/imageConfig.json';
 
 const MovieItem = ({ movie }) => {
   console.log(movie);
   const { backdrop_path, title, vote_average, overview } = movie;
   return (
-    <div className={style.MovieItem} style={{ backgroundImage: 'url(http://image.tmdb.org/t/p/original' + backdrop_path + ')' }} >
+    <div className={style.MovieItem} style={{ backgroundImage: `url(${config.images.secure_base_url}/${config.images.backdrop_size_s}${backdrop_path})` }} >
       <div className={style.overlay}>
         <div className={style.title}>{title}</div>
         <div className={style.rating}>{vote_average} / 10</div>
